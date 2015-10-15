308dd1262a62b69af28d11c08e2eec43c2625527
Adam Kowalewski <github@adamkowalewski.com>
2015-10-15 19:56:30 +0200

    [PRE-1] repositioned licence header

--- opw-pre-inbound-ws-api/src/main/java/pl/otwartapw/opw/pre/inbound/ws/api/ObwodowaApi.java
+++ opw-pre-inbound-ws-api/src/main/java/pl/otwartapw/opw/pre/inbound/ws/api/ObwodowaApi.java
@@ -1,17 +1,3 @@
-package pl.otwartapw.opw.pre.inbound.ws.api;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.MediaType.APPLICATION_XML;
-import javax.ws.rs.core.Response;
-
 /*
  * The MIT License
  *
@@ -35,6 +21,20 @@ import javax.ws.rs.core.Response;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+package pl.otwartapw.opw.pre.inbound.ws.api;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import javax.ws.rs.core.Response;
+
 /**
  * API definition for Komisja Obwodowa resource.
  *
