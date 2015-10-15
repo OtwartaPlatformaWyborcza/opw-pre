f4311b9cca33057cf33a8f787d7a53a06651f0dc
Adam Kowalewski <github@adamkowalewski.com>
2015-10-15 19:56:56 +0200

    [PRE-1] defined API for protokol

---
+++ opw-pre-inbound-ws-api/src/main/java/pl/otwartapw/opw/pre/inbound/ws/api/ProtokolApi.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Otwarta Platforma Wyborcza.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package pl.otwartapw.opw.pre.inbound.ws.api;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import javax.ws.rs.core.Response;
+
+/**
+ * API definition for Protokol Komisji Obwodowej resource.
+ *
+ * @author Adam Kowalewski
+ */
+@Path("/protokol")
+public interface ProtokolApi {
+
+    public static final String PP_PID = "protokolId";
+
+    @GET
+    @Path("/")
+    @Produces({APPLICATION_JSON, APPLICATION_XML})
+    public Response getProtokolList();
+
+    @POST
+    @Path("/")
+    @Consumes({APPLICATION_JSON, APPLICATION_XML})
+    @Produces({APPLICATION_JSON, APPLICATION_XML})
+    public Response postProtokol(@NotNull @Valid ObwodowaProtokolDto dto);
+
+    @GET
+    @Path("/{" + PP_PID + "}")
+    @Produces({APPLICATION_JSON, APPLICATION_XML})
+    public Response getProtokol(@NotNull @Valid @PathParam(PP_PID) String protokolId);
+
+    @PUT
+    @Path("/{" + PP_PID + "}")
+    @Consumes({APPLICATION_JSON, APPLICATION_XML})
+    @Produces({APPLICATION_JSON, APPLICATION_XML})
+    public Response putProtokol(
+            @NotNull @Valid @PathParam(PP_PID) String protokolId,
+            @NotNull @Valid ObwodowaProtokolDto dto);
+
+}
