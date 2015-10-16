a82cccf69c31c1e5243804abc388941f622115e9
Adam Kowalewski <github@adamkowalewski.com>
2015-10-16 08:21:00 +0200

    [PRE-1] defined DTO

--- opw-pre-inbound-ws-api/src/main/java/pl/otwartapw/opw/pre/inbound/ws/api/dto/ProtokolDto.java
+++ opw-pre-inbound-ws-api/src/main/java/pl/otwartapw/opw/pre/inbound/ws/api/dto/ProtokolDto.java
@@ -24,18 +24,157 @@
 package pl.otwartapw.opw.pre.inbound.ws.api.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
+ * DTO represents Protokol Komisji Obwodowej provided by client application.
  *
  * @author Adam Kowalewski
+ * @version 2015.10.16
  */
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ProtokolDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @NotNull
+    private Short uprawnionych;
+    @NotNull
+    private Short glosujacych;
+    @NotNull
+    private Short kartWaznych;
+    @NotNull
+    private Short glosowNieWaznych;
+    @NotNull
+    private Short glosowWaznych;
+
+    private String timestampCreated;
+    private int ratedPositiv;
+    private int ratedNegativ;
+
+    private Map<Integer, Integer> glosyKandydataMap;
+    private List<LinkDto> linkList;
+
+    public ProtokolDto() {
+        this.linkList = new ArrayList<>();
+        this.glosyKandydataMap = new HashMap<>();
+    }
+
+    public ProtokolDto(Short uprawnionych, Short glosujacych, Short kartWaznych, Short glosowNieWaznych, Short glosowWaznych, String timestampCreated, int ratedPositiv, int ratedNegativ, Map<Integer, Integer> glosyKandydataMap, List<LinkDto> linkList) {
+        this.uprawnionych = uprawnionych;
+        this.glosujacych = glosujacych;
+        this.kartWaznych = kartWaznych;
+        this.glosowNieWaznych = glosowNieWaznych;
+        this.glosowWaznych = glosowWaznych;
+        this.timestampCreated = timestampCreated;
+        this.ratedPositiv = ratedPositiv;
+        this.ratedNegativ = ratedNegativ;
+        this.glosyKandydataMap = glosyKandydataMap;
+        this.linkList = linkList;
+    }
+
+    public Short getUprawnionych() {
+        return uprawnionych;
+    }
+
+    public void setUprawnionych(Short uprawnionych) {
+        this.uprawnionych = uprawnionych;
+    }
+
+    public Short getGlosujacych() {
+        return glosujacych;
+    }
+
+    public void setGlosujacych(Short glosujacych) {
+        this.glosujacych = glosujacych;
+    }
+
+    public Short getKartWaznych() {
+        return kartWaznych;
+    }
+
+    public void setKartWaznych(Short kartWaznych) {
+        this.kartWaznych = kartWaznych;
+    }
+
+    public Short getGlosowNieWaznych() {
+        return glosowNieWaznych;
+    }
+
+    public void setGlosowNieWaznych(Short glosowNieWaznych) {
+        this.glosowNieWaznych = glosowNieWaznych;
+    }
+
+    public Short getGlosowWaznych() {
+        return glosowWaznych;
+    }
+
+    public void setGlosowWaznych(Short glosowWaznych) {
+        this.glosowWaznych = glosowWaznych;
+    }
+
+    public String getTimestampCreated() {
+        return timestampCreated;
+    }
+
+    public void setTimestampCreated(String timestampCreated) {
+        this.timestampCreated = timestampCreated;
+    }
+
+    public int getRatedPositiv() {
+        return ratedPositiv;
+    }
+
+    public void setRatedPositiv(int ratedPositiv) {
+        this.ratedPositiv = ratedPositiv;
+    }
+
+    public int getRatedNegativ() {
+        return ratedNegativ;
+    }
+
+    public void setRatedNegativ(int ratedNegativ) {
+        this.ratedNegativ = ratedNegativ;
+    }
+
+    public Map<Integer, Integer> getGlosyKandydataMap() {
+        return glosyKandydataMap;
+    }
+
+    public void setGlosyKandydataMap(Map<Integer, Integer> glosyKandydataMap) {
+        this.glosyKandydataMap = glosyKandydataMap;
+    }
+
+    public List<LinkDto> getLinkList() {
+        return linkList;
+    }
+
+    public void setLinkList(List<LinkDto> linkList) {
+        this.linkList = linkList;
+    }
+
+    @Override
+    public String toString() {
+        return "ProtokolDto{"
+                + "uprawnionych=" + uprawnionych
+                + ", glosujacych=" + glosujacych
+                + ", kartWaznych=" + kartWaznych
+                + ", glosowNieWaznych=" + glosowNieWaznych
+                + ", glosowWaznych=" + glosowWaznych
+                + ", timestampCreated=" + timestampCreated
+                + ", ratedPositiv=" + ratedPositiv
+                + ", ratedNegativ=" + ratedNegativ
+                + ", glosyKandydataMap=" + glosyKandydataMap.size()
+                + ", linkList=" + linkList.size()
+                + '}';
+    }
 
 }
