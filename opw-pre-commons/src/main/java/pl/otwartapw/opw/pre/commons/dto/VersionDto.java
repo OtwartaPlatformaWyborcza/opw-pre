2d918c0f2ddd7e238923b8dc53c9aa3834d1e9a8
Adam Kowalewski <github@adamkowalewski.com>
2015-10-23 19:47:10 +0200

    [PRE-9] defined commons for version

---
+++ opw-pre-commons/src/main/java/pl/otwartapw/opw/pre/commons/dto/VersionDto.java
@@ -0,0 +1,77 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Otwarta Platforma Wyborcza.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package pl.otwartapw.opw.pre.commons.dto;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * DTO represents artifact version.
+ *
+ * @author Adam Kowalewski
+ * @version 2015.10.23
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class VersionDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String artifact;
+    private String version;
+
+    public VersionDto() {
+    }
+
+    public VersionDto(String artifact, String version) {
+        this.artifact = artifact;
+        this.version = version;
+    }
+
+    public String getArtifact() {
+        return artifact;
+    }
+
+    public void setArtifact(String artifact) {
+        this.artifact = artifact;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "VersionDto{"
+                + "artifact=" + artifact
+                + ", version=" + version
+                + '}';
+    }
+
+}
