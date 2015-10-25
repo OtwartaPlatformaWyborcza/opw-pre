46006f42579992d122f458824f0071ebd79dd5dc
Adam Kowalewski <github@adamkowalewski.com>
2015-10-25 20:10:27 +0100

    [PRE-11] defined DTO

--- opw-pre-management-ws-api/src/main/java/pl/otwartapw/opw/pre/management/ws/api/LinkDto.java
+++ opw-pre-management-ws-api/src/main/java/pl/otwartapw/opw/pre/management/ws/api/LinkDto.java
@@ -23,10 +23,81 @@
  */
 package pl.otwartapw.opw.pre.management.ws.api;
 
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
+ * DTO represents Link.
  *
  * @author Adam Kowalewski
+ * @version 2015.10.25
  */
-public class LinkDto {
-    
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class LinkDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String label;
+    private String url;
+    private String comment;
+    private int protokolId;
+    private int userId;
+
+    public LinkDto() {
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getProtokolId() {
+        return protokolId;
+    }
+
+    public void setProtokolId(int protokolId) {
+        this.protokolId = protokolId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "LinkDto{"
+                + "label=" + label
+                + ", url=" + url
+                + ", comment=" + comment
+                + ", protokolId=" + protokolId
+                + ", userId=" + userId
+                + '}';
+    }
+
 }
